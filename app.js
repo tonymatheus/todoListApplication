c5205cd680ae11c69196699a0c6bca1888f5dd62
tonymatheus <tony.chaves14@gmail.com>
2023-05-03 20:28:21 -0300

    chore: remove comments

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-/*
-1 - obter e  armazenar a referencia  do form 
-2- Adicionar um listner de evento que  executa um fun;áo  quando  o  form  for enviado 
-3- armazenar o  valor do input em uma const 
-4- não permitir que  espaços em brancos sejam submetidos
-5- armazenar o valor  da ul
-6- quando  o  form  for  enviado, deve ser inserido na ul uma nova li que contenha o  valor  do input
-7- imlementar uma condição que verifica se o  valor  do input foi preenchido no momento  do  envio do form
-8- limpar o input após o envio 
-9- remover o  elemento  clicado na lixeira
-10- pegar referencia do elemento clicado
-11- colocar uma condicional para verificar quando clicou na lixeira
-12- Converter o elemento  clicado para um array 
-13- criar uma const para para armazenar a referencia do input
-14- filtrar os todos atraves do valor do input e adiconar uma classe nos todos que náo correspondem ao  valor do input 
-15- Obter um array somente com as strings dos todos que correspondem ao valor do input com um filter
-16 - Exibir as lis que contem o valor que foi inserido no input
-17- 
-*/
-
 const formAddTodo = document.querySelector(".form-add-todo");
 const todosContainer = document.querySelector(".todos-container");
 const inputSearchTodo = document.querySelector(".form-search");
